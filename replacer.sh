f87acf1619f87bff6b9ca2138d38e88ae630b464
Olivier Brian <mail@obrian.ch>
2024-12-13 22:34:09 +0100

    update and add tools

--- replacer.sh
+++ replacer.sh
@@ -3,7 +3,7 @@
 # Verzeichnis festlegen (aktuelles Verzeichnis oder spezifisches). Script direkt im Ordner /docs/de oder /docs/en mit dem Befehl "../../convert.sh en" auführen
 
 i18n=$1 #wird als Argument geliefert
-SRC_DIR="../../src/$i18n/"
+SRC_DIR="./en/"
 DES_DIR="./"
 
 declare -A replacements
@@ -19,29 +19,28 @@ replacements=(
   ['Em002-4.1 Checkliste OSS-Community']='[Em002-4.1 Checkliste OSS-Community](em002-4.1.md)'
   ['Em002-5 Faktenblatt EMBAG und OSS']='[Em002-5 Faktenblatt EMBAG und OSS](em002-5.md)'
   ['Em002-6 OSS-FAQ']='[Em002-6 OSS-FAQ](em002-6.md)'
+  ['Em002 Strategic Guidelines for Open Source Software in the Federal Administration']='[Em002 Strategic Guidelines for Open Source Software in the Federal Administration](em002.md)'
+  ['Em002-1 Practical Guidelines for Open Source Software in the Federal Administration']='[Em002-1 Practical Guidelines for Open Source Software in the Federal Administration](em002-1.md)'
+  ['Em002-2 Instructions for Publishing OSS']='[Em002-2 Instructions for Publishing OSS](em002-2.md)'
+  ['Em002-2.1 Preliminary Assessment Checklist']='[Em002-2.1 Preliminary Assessment Checklist](em002-2.1.md)'
+  ['Em002-2.2 Analysis and Preparation Checklist']='[Em002-2.2 Analysis and Preparation Checklist](em002-2.2.md)'
+  ['Em002-2.3 Release and Publication Checklist']='[Em002-2.3 Release and Publication Checklist](em002-2.3.md)'
+  ['Em002-3 OSS Licensing Guidelines']='[Em002-3 OSS Licensing Guidelines](em002-3.md)'
+  ['Em002-4 OSS Community Guidelines']='[Em002-4 OSS Community Guidelines](em002-4.md)'
+  ['Em002-4.1 OSS Community Checklist']='[Em002-4.1 OSS Community Checklist](em002-4.1.md)'
+  ['Em002-5 EMOTA and OSS Factsheet']='[Em002-5 EMOTA and OSS Factsheet](em002-5.md)'
+  ['Em002-6 FAQ on OSS and Art. 9 EMOTA']='[Em002-6 FAQ on OSS and Art. 9 EMOTA](em002-6.md)'
+  
 )
 
 # Schleife durch alle Dateien im Verzeichnis
-for file in "$SRC_DIR"*.docx; do
-  
-  # Basisname ohne Verzeichnis
-  filename="${file##*/}"
-  # Entfernt nur die letzte Extension
-  filename_short=$(echo "$filename" | cut -d ' ' -f 1)
-  # In Kleinbuchstaben umwandel
-  filename_short_lc=$(echo "$filename_short" | tr '[:upper:]' '[:lower:]')
-
-  # Ausgabedateiname erstellen
-  output=$DES_DIR$filename_short_lc".md"
-  echo "Convert $file -> $output"
-  # Pandoc-Befehl ausführen
-  pandoc -f docx -t markdown "$file" -o "$output" -t gfm --extract-media $DES_DIR"assets/""$filename_short_lc"/ --template=../../header_$i18n.md
-
+for file in "$SRC_DIR"*.md; do
+  echo $file
   # Ersetzten von Wörter aus dem Array
   for word in "${!replacements[@]}"; do
-    count=$(grep -o "$word" "$output" | wc -l)
+    count=$(grep -o "$word" "$file" | wc -l)
     echo "Replace word \"$word\" -> $count"
-    sed -i "s/$word/${replacements[$word]}/g" "$output"
+    sed -i "s/$word/${replacements[$word]}/g" "$file"
   done
 
   echo "********************************************************************"
